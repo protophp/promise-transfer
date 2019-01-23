1df7c47eacb25f6616c30b7fd095a929b8fed7a4
Arash Ghamkhari <dev@ghamkhari.com>
2019-01-23 11:59:24 +0330

    Added Transfer's ParserException

---
+++ src/Transfer/Exception/ParserException.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Proto\Socket\Transfer\Exception;
+
+class ParserException extends \Exception
+{
+    const TYPE_NOT_FOUND = 1;
+    const ID_NOT_FOUND = 2;
+    const SEQ_NOT_FOUND = 3;
+    const INVALID_TYPE = 4;
+    const INVALID_ID = 5;
+    const INVALID_SEQ = 6;
+
+    const MSG = [
+        self::TYPE_NOT_FOUND => "Transfer's Type not found!",
+        self::ID_NOT_FOUND => "Transfer's ID not found!",
+        self::SEQ_NOT_FOUND => "Transfer's Seq not found!",
+        self::INVALID_TYPE => "Invalid transfer's Type!",
+        self::INVALID_ID => "Invalid transfer's ID!",
+        self::INVALID_SEQ => "Invalid transfer's Seq!",
+    ];
+
+    public function getMsg(): string
+    {
+        return isset(self::MSG[$this->code]) ? self::MSG[$this->code] : '';
+    }
+}
