c19d0c4dc16f4d665d5bcd279b35c7e97ec56af5
Arash Ghamkhari <dev@ghamkhari.com>
2019-01-26 13:37:03 +0330

    The response test is written to TransferTest.

--- tests/TransferTest.php
+++ tests/TransferTest.php
@@ -7,6 +7,7 @@ use Monolog\Logger;
 use Proto\Pack\Pack;
 use Proto\Pack\PackInterface;
 use Proto\Socket\Tests\Stub\ConnectionStub;
+use Proto\Socket\Transfer\ParserInterface;
 use Proto\Socket\Transfer\Transfer;
 use Proto\Socket\Transfer\TransferInterface;
 
@@ -35,15 +36,16 @@ class TransferTest extends TestCase
         $cTransfer->init($this->clientSession);
 
         $sTransfer->on('established', function (TransferInterface $transfer) use ($sConn) {
-            $transfer->on('data', function (PackInterface $pack) use ($sConn) {
+            $transfer->on('data', function (PackInterface $pack, ParserInterface $parser) use ($sConn) {
+                $this->assertFalse($parser->isWaitForResponse());
                 $this->assertSame('FOO-BAR', $pack->getData());
                 $sConn->flush();
             });
         });
 
         $cTransfer->on('established', function (TransferInterface $transfer) use ($cConn) {
-            $transfer->send((new Pack())->setData('FOO-BAR'), function () {
-                $this->assertEquals(1, $this->getCount());
+            $transfer->send((new Pack())->setData('FOO-BAR'), null, function () {
+                $this->assertEquals(2, $this->getCount());
             });
             $cConn->flush();
         });
@@ -51,6 +53,57 @@ class TransferTest extends TestCase
         $cConn->flush();
         $sConn->flush();
 
-        $this->assertEquals(2, $this->getCount());
+        $this->assertEquals(3, $this->getCount());
+    }
+
+    public function testResponse()
+    {
+        // Create new connection
+        $sConn = new ConnectionStub();
+        $cConn = new ConnectionStub();
+        $cConn->connect($sConn);
+
+        $sConn->setLogger(new Logger('ServerConn', [new ErrorLogHandler()]));
+        $cConn->setLogger(new Logger('ClientConn', [new ErrorLogHandler()]));
+
+        // Setup the transfer
+        $sTransfer = new Transfer($sConn, $this->sessionManager);
+        $cTransfer = new Transfer($cConn, $this->sessionManager);
+
+        $sTransfer->setLogger(new Logger('ServerTransfer', [new ErrorLogHandler()]));
+        $cTransfer->setLogger(new Logger('ClientTransfer', [new ErrorLogHandler()]));
+
+        // Init the transfer
+        $sTransfer->init();
+        $cTransfer->init($this->clientSession);
+
+        $sTransfer->on('established', function (TransferInterface $transfer) use ($sConn, $sTransfer) {
+            $transfer->on('data', function (PackInterface $pack, ParserInterface $parser) use ($sConn, $sTransfer) {
+                $this->assertTrue($parser->isWaitForResponse());
+                $this->assertSame('FOO-BAR', $pack->getData());
+
+                // send response
+                $sTransfer->response((new Pack())->setData('RESPONSE-FOO-BAR'), $parser->getId());
+
+                $sConn->flush();
+            });
+        });
+
+        $cTransfer->on('established', function (TransferInterface $transfer) use ($cConn) {
+            $transfer->send(
+                (new Pack())->setData('FOO-BAR'),
+                function (PackInterface $pack) {
+                    $this->assertSame('RESPONSE-FOO-BAR', $pack->getData());
+                },
+                function () {
+                    $this->assertTrue(true);
+                });
+            $cConn->flush();
+        });
+
+        $cConn->flush();
+        $sConn->flush();
+
+        $this->assertEquals(4, $this->getCount());
     }
 }
